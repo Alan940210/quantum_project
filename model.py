cdb734f42cef197cc8020f3686faffe4c9b51711
Alan <alan909680463@gmail.com>
2026-04-16 00:00:49 +0800

    1st_quantum_edition

--- model.py
+++ model.py
@@ -1,27 +1,40 @@
 import torch.nn as nn
+import pennylane as qml
+
+dev = qml.device("default.qubit", wires=8)
+@qml.qnode(dev, interface="torch")
+def circuit(inputs, weights):
+    qml.AngleEmbedding(inputs, wires=range(8))
+    qml.StronglyEntanglingLayers(weights[0], wires=range(4))
+    qml.StronglyEntanglingLayers(weights[1], wires=range(4, 8))
+
+    return [qml.expval(qml.PauliZ(i)) for i in range(8)]
+
+shapes = {"weights" : (2, 1, 4, 3)}
 
 class CNNModel(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),  # 1x28x28 -> 32x28x28
+        self.DC = nn.Sequential(
+            nn.Conv2d(1, 8, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2),                             # -> 32x14x14
+            nn.MaxPool2d(2),                             # -> 8 * 14 * 14
 
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),# -> 64x14x14
+            nn.Conv2d(8, 8, kernel_size=3, padding=1),
             nn.ReLU(),
-            nn.MaxPool2d(2)                              # -> 64x7x7
-        )
+            nn.MaxPool2d(2),                             # -> 8 * 7 * 7
 
-        self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(64 * 7 * 7, 128),
-            nn.ReLU(),
-            nn.Linear(128, 10)
+            nn.Linear(8 * 7 * 7, 8),
         )
+
+        self.qlayer = qml.qnn.TorchLayer(circuit, shapes)
+
+        self.end = nn.Linear(8, 10)
 
     def forward(self, x):
-        x = self.conv(x)
-        x = self.fc(x)
+        x = self.DC(x)
+        x = self.qlayer(x)
+        x = self.end(x)
         return x
